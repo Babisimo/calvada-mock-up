901c863c51c4a9f6f779e58369f576d66035da69
Babisimo <gondaniel35@yahoo.com>
2022-07-01 21:14:14 -0700

    changed name from Styles to styled due to vercel issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './pages/Styles/counties.css'
-import './pages/Styles/stateStyle.css'
+import './pages/styles/counties.css'
+import './pages/styles/stateStyle.css'
 import App from './App';
 import './style.css'
-import './pages/Styles/contact.css'
+import './pages/styles/contact.css'
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
