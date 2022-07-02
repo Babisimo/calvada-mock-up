fd949ab914fcadcf177ba0ee5413e942783848c0
Babisimo <gondaniel35@yahoo.com>
2022-07-01 21:49:08 -0700

    Changed fima (image that was not working)

--- src/pages/Main/Links.js
+++ src/pages/Main/Links.js
@@ -4,7 +4,7 @@ export default function Links() {
     return (
         <div className="links">
             <h2>Links</h2>
-            <a href="https://msc.fema.gov/portal/home" target={"_blank"} rel="noreferrer"><img alt="FEMA" src={require("C:/Users/gonda/Desktop/calvada-react/calvada-mock-up/src/imgs/fema-logo-main.png")} width="150px"></img></a>
+            <a href="https://msc.fema.gov/portal/home" target={"_blank"} rel="noreferrer"><img alt="FEMA" src={require("C:/Users/gonda/Desktop/calvada-react/calvada-mock-up/src/imgs/fema.png")} width="150px"></img></a>
             <a href="https://www.ngs.noaa.gov/cgi-bin/ds_radius.prl" target={"_blank"} rel="noreferrer"><img alt="BY POINT RADIUS" src={require("C:/Users/gonda/Desktop/calvada-react/calvada-mock-up/src/imgs/NGSbut.png")} width="60px"></img></a>
             <a href="https://www.ngdc.noaa.gov/geomag/calculators/magcalc.shtml" target={"_blank"} rel="noreferrer"><img alt="NOAA" src={require("C:/Users/gonda/Desktop/calvada-react/calvada-mock-up/src/imgs/nesdisngdcleft.png")}></img></a>
             <a href="https://www.earthpoint.us/Townships.aspx" target={"_blank"} rel="noreferrer">Township and Range - Public Land Survey System</a>
