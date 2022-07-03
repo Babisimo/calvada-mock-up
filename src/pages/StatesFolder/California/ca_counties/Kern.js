aa4d23ebcf881df1f92ba8de21c07455e313f831
Babisimo <gondaniel35@yahoo.com>
2022-07-03 13:11:34 -0700

    Added to Kings and Kern

--- src/pages/StatesFolder/California/ca_counties/Kern.js
+++ src/pages/StatesFolder/California/ca_counties/Kern.js
@@ -24,6 +24,16 @@ const Kern = () => {
 
       <span className='span'>
         <h3>Links:</h3>
+        <a href='https://www.kerncounty.com/' target={"_blank"} rel='noreferrer'>Official Site</a>
+        <br />
+        <br />
+        <a href='http://assessor.co.kern.ca.us/prop_search.php' target={"_blank"} rel='noreferrer'>Assessor's Office</a>&nbsp;&nbsp;&nbsp;(Link Not Working)
+        <br />
+        <br />
+        <a href='https://kernpublicworks.com/maps/' target={"_blank"} rel='noreferrer'>Record Maps</a>&nbsp;&nbsp;&nbsp;(Free Maps)
+        <br />
+        <br />
+        <a href='https://maps.kerncounty.com/H5/index.html?viewer=KCPublic' target={"_blank"} rel='noreferrer'>GIS Mapping</a>&nbsp;&nbsp;&nbsp;(Free Record Maps)
 
       </span>
 
@@ -33,6 +43,17 @@ const Kern = () => {
 
       <span className='span'>
         <h3>Public Works:</h3>
+        <h4>Engineering & Survey Services Department</h4>
+        <p>Charles Lackey: Director</p>
+        <br />
+        <h4>Public Services Building Address:</h4>
+        <p>2700 "M" Street, Suite 570</p>
+        <p>Bakersfield, California 93301-2370</p>
+        <p>Monday - Friday 8:00 AM - 5:00 PM</p>
+        <p>Phone: (661) 862-5510</p>
+        <p>Fax: (661) 862-5101</p>
+        <p>TTY Relay: 1-800-735-2929</p>
+        <p>Email: ess@co.kern.ca.us</p>
 
       </span>
 
@@ -42,6 +63,15 @@ const Kern = () => {
 
       <span className='span'>
         <h3>Caltrans:</h3>
+        <a href='http://www.dot.ca.gov/dist6/' target={"_blank"} rel='noreferrer'>Caltrans District 6</a>&nbsp;&nbsp;&nbsp;(LINK NOT WORKING)
+        <p>Fresno Fair Grounds, Educational Building</p>
+        <p>1121 S. Chance Avenue, Fresno California 93702</p>
+
+        <h4>Telephone:</h4>
+        <p>(559) 444-2409 (Fresno/Madera)</p>
+        <p>(559) 488-4082 (Tulare/Kings)</p>
+        <p>(661) 326-3968 (Kern)</p>
+        <p>(559) 488-4067</p>
 
       </span>
 
--- src/pages/StatesFolder/California/ca_counties/Kings.js
+++ src/pages/StatesFolder/California/ca_counties/Kings.js
@@ -1,8 +1,64 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Kings = () => {
   return (
-    <div>Kings</div>
+    <div className='county-info'>
+      <div className='county-btn'>
+        <Link to={'/counties'}><button>Back to Counties</button></Link>
+        <h1 className='title'>Kings County</h1>
+      </div>
+
+      {/* Border To Separate Information */}
+      <p className='border'></p>
+      {/* Border To Separate Information */}
+
+      <div className='apn-format'>
+        <h3>APN Format:</h3>
+        <p>020-017-013-000</p>
+      </div>
+
+      {/* Border To Separate Information */}
+      <p className='border'></p>
+      {/* Border To Separate Information */}
+
+      <span className='span'>
+        <h3>Links:</h3>
+        <a href='https://assr.parcelquest.com/Home/Disclaimer' target={"_blank"} rel='noreferrer'>Parcel Search</a>&nbsp;&nbsp;&nbsp;(Free APNS)
+      </span>
+
+      {/* Border To Separate Information */}
+      <p className='border'></p>
+      {/* Border To Separate Information */}
+
+      <span className='span'>
+        <h3>Public Works:</h3>
+
+      </span>
+
+      {/* Border To Separate Information */}
+      <p className='border'></p>
+      {/* Border To Separate Information */}
+
+      <span className='span'>
+        <h3>Caltrans:</h3>
+        <a href='http://www.dot.ca.gov/dist6/' target={"_blank"} rel='noreferrer'>Caltrans District 6</a>&nbsp;&nbsp;&nbsp;(LINK NOT WORKING)
+        <p>Fresno Fair Grounds, Educational Building</p>
+        <p>1121 S. Chance Avenue, Fresno California 93702</p>
+
+        <h4>Telephone:</h4>
+        <p>(559) 444-2409 (Fresno/Madera)</p>
+        <p>(559) 488-4082 (Tulare/Kings)</p>
+        <p>(661) 326-3968 (Kern)</p>
+        <p>(559) 488-4067</p>
+      </span>
+
+      {/* Border To Separate Information */}
+      <p className='border'></p>
+      {/* Border To Separate Information */}
+
+
+    </div>
   )
 }
 
