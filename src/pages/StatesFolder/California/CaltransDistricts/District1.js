1870dce3a840914e6ec86ba0f70e3982210ff48f
Babisimo <gondaniel35@yahoo.com>
2022-07-30 17:36:44 -0700

    Added title to Districts

--- src/pages/StatesFolder/California/CaltransDistricts/District1.js
+++ src/pages/StatesFolder/California/CaltransDistricts/District1.js
@@ -3,6 +3,7 @@ import React from 'react'
 function District1() {
     return (
         <div>
+            <br />
             <a href='https://dot.ca.gov/Configuration/Error-Pages/Error-404-Page?item=%2fdist1%2f&user=extranet%5cAnonymous&site=Caltrans' target={"_blank"} rel='noreferrer'>Caltrans District 1</a>&nbsp;&nbsp;&nbsp;(Link Not Working)
         </div>
     )
