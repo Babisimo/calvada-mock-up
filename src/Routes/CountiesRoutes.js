6f86ea31a8b04d4c7305f183397e56de385bc3ff
Babisimo <gondaniel35@yahoo.com>
2023-04-21 11:08:30 -0700

    Created CACountiesArr to make it easier to update

--- src/Routes/CountiesRoutes.js
+++ src/Routes/CountiesRoutes.js
@@ -1,8 +1,99 @@
+// import React from "react";
+// import { Route, Routes } from 'react-router-dom'
+
+// // Importing California Counties
+// import counties from "../pages/StatesFolder/California/CA_Components/CACountiesExports";
+
+// // Importing Arizona Counties
+// import { Maricopa, Pima, Yuma } from '../pages/StatesFolder/Arizona/AZ_Components/az_exports'
+
+// // Importing Nevada Counties
+// import { Clark, Washoe } from '../pages/StatesFolder/Nevada/NV_Components/nv_exports'
+
+// // Importing Texas Counties
+// import { Bexar } from '../pages/StatesFolder/Texas/TX_Components/tx_exports'
+
+
+// export default function CountiesRoutes() {
+//     return (
+//         <>
+//             {/* California Counties Routes */}
+//             <Routes>
+//                 <Route path="/alameda" element={<cacounties.Alameda />} />
+//                 <Route path="/amador" element={<cacounties.Amador />} />
+//                 <Route path="/alpine" element={<cacounties.Alpine />} />
+//                 <Route path="/butte" element={<cacounties.Butte />} />
+//                 <Route path="/calaveras" element={<cacounties.Calaveras />} />
+//                 <Route path="/contracosta" element={<cacounties.ContraCosta />} />
+//                 <Route path="/eldorado" element={<cacounties.ElDorado />} />
+//                 <Route path="/fresno" element={<cacounties.Fresno />} />
+//                 <Route path="/imperial" element={<cacounties.Imperial />} />
+//                 <Route path="/kern" element={<cacounties.Kern />} />
+//                 <Route path="/kings" element={<cacounties.Kings />} />
+//                 <Route path="/lake" element={<cacounties.Lake />} />
+//                 <Route path="/lassen" element={<cacounties.Lassen />} />
+//                 <Route path="/losangeles" element={<cacounties.LosAngeles />} />
+//                 <Route path="/marin" element={<cacounties.Marin />} />
+//                 <Route path="/modoc" element={<cacounties.Modoc />} />
+//                 <Route path="/mono" element={<cacounties.Mono />} />
+//                 <Route path="/merced" element={<cacounties.Merced />} />
+//                 <Route path="/madera" element={<cacounties.Madera />} />
+//                 <Route path="/monterey" element={<cacounties.Monterey />} />
+//                 <Route path="/nevada" element={<cacounties.Nevada />} />
+//                 <Route path="/orange" element={<cacounties.Orange />} />
+//                 <Route path="/placer" element={<cacounties.Placer />} />
+//                 <Route path="/plumas" element={<cacounties.Plumas />} />
+//                 <Route path="/riverside" element={<cacounties.Riverside />} />
+//                 <Route path="/sacramento" element={<cacounties.Sacramento />} />
+//                 <Route path="/sanbernardino" element={<cacounties.SanBernardino />} />
+//                 <Route path="/sandiego" element={<cacounties.SanDiego />} />
+//                 <Route path="/sanfransisco" element={<cacounties.SanFrancisco />} />
+//                 <Route path="/sanjoaquin" element={<cacounties.SanJoaquin />} />
+//                 <Route path="/sanluisobispo" element={<cacounties.SanLuisObispo />} />
+//                 <Route path="/sanmateo" element={<cacounties.SanMateo />} />
+//                 <Route path="/santabarbara" element={<cacounties.SantaBarbara />} />
+//                 <Route path="/santaclara" element={<cacounties.SantaClara />} />
+//                 <Route path="/santacruz" element={<cacounties.SantaCruz />} />
+//                 <Route path="/shasta" element={<cacounties.Shasta />} />
+//                 <Route path="/solano" element={<cacounties.Solano />} />
+//                 <Route path="/sonoma" element={<cacounties.Sonoma />} />
+//                 <Route path="/stanislaus" element={<cacounties.Stanislaus />} />
+//                 <Route path="/sierra" element={<cacounties.Sierra />} />
+//                 <Route path="/sutter" element={<cacounties.Sutter />} />
+//                 <Route path="/siskiyou" element={<cacounties.Siskiyou />} />
+//                 <Route path="/tulare" element={<cacounties.Tulare />} />
+//                 <Route path="/tehama" element={<cacounties.Tehama />} />
+//                 <Route path="/trinity" element={<cacounties.Trinity />} />
+//                 <Route path="/ventura" element={<cacounties.Ventura />} />
+//                 <Route path="/yolo" element={<cacounties.Yolo />} />
+//                 <Route path="/yuba" element={<cacounties.Yuba />} />
+//             </Routes>
+
+//             {/* Arizona Counties Routes */}
+//             <Routes>
+//                 <Route path="/maricopa" element={<Maricopa />} />
+//                 <Route path="/pima" element={<Pima />} />
+//                 <Route path="/yuma" element={<Yuma />} />
+//             </Routes>
+
+//             {/* Nevada Counties Routes */}
+//             <Routes>
+//                 <Route path="/clark" element={<Clark />} />
+//                 <Route path="/washoe" element={<Washoe />} />
+//             </Routes>
+
+//             {/* Texas Counties Routes */}
+//             <Routes>
+//                 <Route path="/bexar" element={<Bexar />} />
+//             </Routes>
+//         </>
+//     )
+// }
 import React from "react";
 import { Route, Routes } from 'react-router-dom'
 
 // Importing California Counties
-import * as cacounties from "../pages/StatesFolder/California/CA_Components/CACountiesExports"
+import counties from "../pages/StatesFolder/California/CA_Components/CACountiesExports";
 
 // Importing Arizona Counties
 import { Maricopa, Pima, Yuma } from '../pages/StatesFolder/Arizona/AZ_Components/az_exports'
@@ -13,60 +104,14 @@ import { Clark, Washoe } from '../pages/StatesFolder/Nevada/NV_Components/nv_exp
 // Importing Texas Counties
 import { Bexar } from '../pages/StatesFolder/Texas/TX_Components/tx_exports'
 
-
 export default function CountiesRoutes() {
     return (
         <>
             {/* California Counties Routes */}
             <Routes>
-                <Route path="/alameda" element={<cacounties.Alameda />} />
-                <Route path="/amador" element={<cacounties.Amador />} />
-                <Route path="/alpine" element={<cacounties.Alpine />} />
-                <Route path="/butte" element={<cacounties.Butte />} />
-                <Route path="/calaveras" element={<cacounties.Calaveras />} />
-                <Route path="/contracosta" element={<cacounties.ContraCosta />} />
-                <Route path="/eldorado" element={<cacounties.ElDorado />} />
-                <Route path="/fresno" element={<cacounties.Fresno />} />
-                <Route path="/imperial" element={<cacounties.Imperial />} />
-                <Route path="/kern" element={<cacounties.Kern />} />
-                <Route path="/kings" element={<cacounties.Kings />} />
-                <Route path="/lake" element={<cacounties.Lake />} />
-                <Route path="/lassen" element={<cacounties.Lassen />} />
-                <Route path="/losangeles" element={<cacounties.LosAngeles />} />
-                <Route path="/marin" element={<cacounties.Marin />} />
-                <Route path="/modoc" element={<cacounties.Modoc />} />
-                <Route path="/mono" element={<cacounties.Mono />} />
-                <Route path="/merced" element={<cacounties.Merced />} />
-                <Route path="/madera" element={<cacounties.Madera />} />
-                <Route path="/monterey" element={<cacounties.Monterey />} />
-                <Route path="/nevada" element={<cacounties.Nevada />} />
-                <Route path="/orange" element={<cacounties.Orange />} />
-                <Route path="/placer" element={<cacounties.Placer />} />
-                <Route path="/plumas" element={<cacounties.Plumas />} />
-                <Route path="/riverside" element={<cacounties.Riverside />} />
-                <Route path="/sacramento" element={<cacounties.Sacramento />} />
-                <Route path="/sanbernardino" element={<cacounties.SanBernardino />} />
-                <Route path="/sandiego" element={<cacounties.SanDiego />} />
-                <Route path="/sanfransisco" element={<cacounties.SanFrancisco />} />
-                <Route path="/sanjoaquin" element={<cacounties.SanJoaquin />} />
-                <Route path="/sanluisobispo" element={<cacounties.SanLuisObispo />} />
-                <Route path="/sanmateo" element={<cacounties.SanMateo />} />
-                <Route path="/santabarbara" element={<cacounties.SantaBarbara />} />
-                <Route path="/santaclara" element={<cacounties.SantaClara />} />
-                <Route path="/santacruz" element={<cacounties.SantaCruz />} />
-                <Route path="/shasta" element={<cacounties.Shasta />} />
-                <Route path="/solano" element={<cacounties.Solano />} />
-                <Route path="/sonoma" element={<cacounties.Sonoma />} />
-                <Route path="/stanislaus" element={<cacounties.Stanislaus />} />
-                <Route path="/sierra" element={<cacounties.Sierra />} />
-                <Route path="/sutter" element={<cacounties.Sutter />} />
-                <Route path="/siskiyou" element={<cacounties.Siskiyou />} />
-                <Route path="/tulare" element={<cacounties.Tulare />} />
-                <Route path="/tehama" element={<cacounties.Tehama />} />
-                <Route path="/trinity" element={<cacounties.Trinity />} />
-                <Route path="/ventura" element={<cacounties.Ventura />} />
-                <Route path="/yolo" element={<cacounties.Yolo />} />
-                <Route path="/yuba" element={<cacounties.Yuba />} />
+                {Object.keys(counties).map(county => (
+                    <Route key={county} path={`/${county.toLowerCase()}`} element={React.createElement(counties[county])} />
+                ))}
             </Routes>
 
             {/* Arizona Counties Routes */}
--- src/pages/StatesFolder/California/CA_Components/CaCountiesNavBar.js
+++ src/pages/StatesFolder/California/CA_Components/CaCountiesNavBar.js
@@ -1,76 +1,39 @@
 import React from "react"
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import CACountiesArr from "../CA_Components/CACountiesArr"
 
 export default function CaCountiesNavBar() {
-
-    let customLinkList = [
-                <CustomLink key={'alameda'} to="/alameda">Alameda</CustomLink>,
-                <CustomLink key={'alpine'} to="/alpine">Alpine</CustomLink>,
-                <CustomLink key={'amador'} to="/amador">Amador</CustomLink>,
-                <CustomLink key={'butte'} to="/butte">Butte</CustomLink>,
-                <CustomLink key={'contra costa'} to="/contracosta">Contra Costa</CustomLink>,
-                <CustomLink key={'calaveras'} to="/calaveras">Calaveras</CustomLink>,
-                <CustomLink key={'el dorado'} to="/eldorado">El Dorado</CustomLink>,
-                <CustomLink key={'fresno'} to="/fresno">Fresno</CustomLink>,
-                <CustomLink key={'imperial'} to="/imperial">Imperial</CustomLink>,
-                <CustomLink key={'kern'} to="/kern">Kern</CustomLink>,
-                <CustomLink key={'kings'} to="/kings">Kings</CustomLink>,
-                <CustomLink key={'lake'} to="/lake">Lake</CustomLink>,
-                <CustomLink key={'lassen'} to="/lassen">Lassen</CustomLink>,
-                <CustomLink key={'los angeles'} to="/losangeles">Los Angeles</CustomLink>,
-                <CustomLink key={'marin'} to="/marin">Marin</CustomLink>,
-                <CustomLink key={'madera'} to="/madera">Madera</CustomLink>,
-                <CustomLink key={'modoc'} to="/modoc">Modoc</CustomLink>,
-                <CustomLink key={'merced'} to="/merced">Merced</CustomLink>,
-                <CustomLink key={'monterey'} to="/monterey">Monterey</CustomLink>,
-                <CustomLink key={'mono'} to="/mono">Mono</CustomLink>,
-                <CustomLink key={'nevada'} to="/nevada">Nevada</CustomLink>,
-                <CustomLink key={'orange'} to="/orange">Orange</CustomLink>,
-                <CustomLink key={'placer'} to="/placer">Placer</CustomLink>,
-                <CustomLink key={'plumas'} to="/plumas">Plumas</CustomLink>,
-                <CustomLink key={'riverside'} to="/riverside">Riverside</CustomLink>,
-                <CustomLink key={'sacranento'} to="/sacramento">Sacramento</CustomLink>,
-                <CustomLink key={'san bernardino'} to="/sanbernardino">San Bernardino</CustomLink>,
-                <CustomLink key={'san diego'} to="/sandiego">San Diego</CustomLink>,
-                <CustomLink key={'san fransisco'} to="/sanfransisco">San Francisco</CustomLink>,
-                <CustomLink key={'san joaquin'} to="/sanjoaquin">San Joaquin</CustomLink>,
-                <CustomLink key={'san luis obispo'} to="/sanluisobispo">San Luis Obispo</CustomLink>,
-                <CustomLink key={'san mateo'} to="/sanmateo">San Mateo</CustomLink>,
-                <CustomLink key={'santa barbara'} to="/santabarbara">Santa Barbara</CustomLink>,
-                <CustomLink key={'santa clara'} to="/santaclara">Santa Clara</CustomLink>,
-                <CustomLink key={'santa cruz'} to="/santacruz">Santa Cruz</CustomLink>,
-                <CustomLink key={'shasta'} to="/shasta">Shasta</CustomLink>,
-                <CustomLink key={'solano'} to="/solano">Solano</CustomLink>,
-                <CustomLink key={'sonoma'} to="/sonoma">Sonoma</CustomLink>,
-                <CustomLink key={'stanislaus'} to="/stanislaus">Stanislaus</CustomLink>,
-                <CustomLink key={'siskiyou'} to="/siskiyou">Siskiyou</CustomLink>,
-                <CustomLink key={'sierra'} to="/sierra">Sierra</CustomLink>,
-                <CustomLink key={'sutter'} to="/sutter">Sutter</CustomLink>,
-                <CustomLink key={'tulare'} to="/tulare">Tulare</CustomLink>,
-                <CustomLink key={'tehama'} to="/tehama">Tehama</CustomLink>,
-                <CustomLink key={'trinity'} to="/trinity">Trinity</CustomLink>,
-                <CustomLink key={'ventura'} to="/ventura">Ventura</CustomLink>,
-                <CustomLink key={'yolo'} to="/yolo">Yolo</CustomLink>,
-                <CustomLink key={'yuba'} to="/yuba">Yuba</CustomLink>
-            ]
-
+  const customLinkList = CACountiesArr.map(county => {
+    const countyName = county.replace(/([A-Z])/g, ' $1').trim(); // Add space between camel case words
     return (
-        <nav className="counties">
-            <ul>
-                {customLinkList.sort((a, b) => {
-                    return a.key.localeCompare(b.key)
-                })}
-            </ul>
-        </nav>
-    )
+      <CustomLink key={county} to={`/${county}`}>
+        {countyName}
+      </CustomLink>
+    );
+  });
+
+  return (
+    <nav className="counties">
+      <ul>
+        {customLinkList.sort((a, b) => {
+          return a.key.localeCompare(b.key);
+        })}
+      </ul>
+    </nav>
+  );
 }
 
 function CustomLink({ to, children, ...props }) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-    return (
-        <li className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>{children}</Link>
-        </li>
-    )
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
 }
+
+
+
