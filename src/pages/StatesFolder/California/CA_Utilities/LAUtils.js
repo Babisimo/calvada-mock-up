05ce4c9c7d3b10e20366186284f3bab401dc79de
Babisimo <gondaniel35@yahoo.com>
2023-09-25 18:03:35 -0700

    Storm drain GIS"

--- src/pages/StatesFolder/California/CA_Utilities/LAUtils.js
+++ src/pages/StatesFolder/California/CA_Utilities/LAUtils.js
@@ -20,8 +20,7 @@ const LAUtils = () => {
           <a href='https://pw.lacounty.gov/smd/sewernetwork/' target='_blank' rel='noreferrer'>LA County Sanitary Sewer Network</a>&nbsp;&nbsp;&nbsp;(County)
           <br />
           <a href='https://data.lacity.org/City-Infrastructure-Service-Requests/Sewer-System/7aty-5ywx' target='_blank' rel='noreferrer'>LA Sewer System</a>&nbsp;&nbsp;&nbsp;(County)
-          <br />
-          <br />
+                    <br />
           <a href='https://www.arcgis.com/apps/mapviewer/index.html?layers=40bfe69666a34f6a8c6a16116715ea02' target='_blank' rel='noreferrer'>El Monte</a>&nbsp;&nbsp;&nbsp;(City Sewer GIS)
           <br />
           <a href='https://www.elsegundo.org/home/showpublisheddocument/1017/637136686386330000' target='_blank' rel='noreferrer'>El Segundo</a>&nbsp;&nbsp;&nbsp;(City Sewer PDF)
@@ -42,6 +41,8 @@ const LAUtils = () => {
         <span className='span'>
           <h3 className='util-header'>Storm Drain Plans</h3>
           <a href='https://drive.google.com/drive/folders/1CQsQZAI8JlWBz4ZsQuovogNhWfWNmR9M?usp=share_link' target='_blank' rel='noreferrer'>LA Storm Drains</a>&nbsp;&nbsp;&nbsp;(Sewer Maps Drive PDFs)
+          <br />
+          <a href='https://pw.lacounty.gov/fcd/StormDrain/disclaimer.cfm?CFID=29218589&CFTOKEN=c3d928abbc8a0ed0-0399C76E-DD3C-677E-6B672BB64A253299' target='_blank' rel='noreferrer'>LA County Storm Drain</a>&nbsp;&nbsp;&nbsp;(GIS)
         </span>
 
         {/* Border To Separate Information */}
