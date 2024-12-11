82812ef3359e01829b604920c67cb3b4027a59e0
babisimo <gondaniel35@yahoo.com>
2024-12-11 10:13:13 -0800

    Adding AZ cities, adding new styling for cities

--- src/pages/StatesFolder/California/CA_Cities/RanchoCucamonga.js
+++ src/pages/StatesFolder/California/CA_Cities/RanchoCucamonga.js
@@ -3,7 +3,7 @@ import React from 'react'
 function RanchoCucamonga() {
     return (
         <div className='city-dropdown'>
-            <button className='city-dropbtn'>Rancho<br/>Cucamonga</button>
+            <button className='city-dropbtn'>Rancho Cucamonga</button>
             <div className='city-dropdown-content'>
                 <a href='https://www.dropbox.com/sh/odwx5bpuycleudn/AAAeD7d2GMBRWAxXJhIdqkqya?dl=0&preview=Benchmarks.pdf' target={'_blank'} rel='noreferrer'>Benchmarks</a>
                 <a href='https://www.dropbox.com/sh/odwx5bpuycleudn/AAB5HmQox9-1Cshbg62a1k-Pa/Centerline%20Tie%20Books?dl=0&subfolder_nav_tracking=1' target={'_blank'} rel='noreferrer'>CL Tie Books</a>
--- src/pages/StatesFolder/California/CA_Cities/SouthSanFrancisco.js
+++ src/pages/StatesFolder/California/CA_Cities/SouthSanFrancisco.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SouthSanFrancisco() {
     return (
         <div className='city-dropdown'>
-            <button className='city-dropbtn'>South <br/> San Francisco</button>
+            <button className='city-dropbtn'>South San Francisco</button>
             <div className='city-dropdown-content'>
                 <a href='https://sfpublicworks.org/ccsf-geodetic-network' target={'_blank'} rel='noreferrer'>CCSF Geodetic Network</a>
             </div>
